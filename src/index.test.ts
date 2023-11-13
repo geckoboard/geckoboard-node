2a98c48cc08d74d569b51e8c6008746da8f04add
Tom Hirst <tom@beard.io>
2023-11-13 14:15:17 +0000

    install prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
-import Geckoboard from './index'
+import Geckoboard from './index';
 
 describe('Geckoboard', () => {
-    it('has the version set correctly', () => {
-        const gb = new Geckoboard('API_KEY')
-        expect(gb.version).toBe('2.0.0')
-    })
-})
+  it('has the version set correctly', () => {
+    const gb = new Geckoboard('API_KEY');
+    expect(gb.version).toBe('2.0.0');
+  });
+});
