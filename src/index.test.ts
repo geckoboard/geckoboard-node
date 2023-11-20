f31123c96b23afd65abdfc625fcf4730cb98610e
Tom Hirst <tom@beard.io>
2023-11-21 02:41:34 +1200

    Include more rigorous tests and documentation for dates

--- src/index.test.ts
+++ src/index.test.ts
@@ -253,63 +253,159 @@ describe('Geckoboard', () => {
     );
   });
 
-  it('can append data to a dataset using the Date object', async () => {
-    const serverSpy = jest.fn().mockReturnValue('{}');
-    mockPool
-      .intercept({
-        method: 'POST',
-        path: '/datasets/steps.by.day/data',
-      })
-      .reply(200, serverSpy);
-    const dataset = prepareDataset();
-    await dataset.append([
-      {
-        timestamp: new Date('2018-01-01T12:00:00Z'),
-        day: new Date('2018-01-01'),
-        steps: 819,
-      },
-      {
-        timestamp: new Date('2018-01-02T12:00:00Z'),
-        day: new Date('2018-01-02'),
-        steps: 409,
-      },
-      {
-        timestamp: new Date('2018-01-03T12:00:00Z'),
-        day: new Date('2018-01-03'),
-        steps: 164,
-      },
-    ]);
-    expect(serverSpy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        method: 'POST',
-        path: '/datasets/steps.by.day/data',
-        headers: expect.objectContaining({
-          authorization: `Basic ${btoa('API_KEY:')}`,
-          'user-agent': 'Geckoboard Node Client 2.0.0',
-          'content-type': 'application/json',
+  it.each([
+    {
+      datetime: new Date('2018'),
+      expected: '2018-01-01T00:00:00.000Z',
+      description: 'just a year string',
+    },
+    {
+      datetime: new Date('2018-02'),
+      expected: '2018-02-01T00:00:00.000Z',
+      description: 'just a month string',
+    },
+    {
+      datetime: new Date('2018-03-02'),
+      expected: '2018-03-02T00:00:00.000Z',
+      description: 'just a date string',
+    },
+    {
+      datetime: new Date('2018-04-05T12:00:00.000Z'),
+      expected: '2018-04-05T12:00:00.000Z',
+      description: 'a UTC datetime string',
+    },
+    {
+      datetime: new Date('2013-09-15T05:53:00+08:00'),
+      expected: '2013-09-14T21:53:00.000Z',
+      description: 'a datetime string with timezone information (+ positve)',
+    },
+    {
+      datetime: new Date('2018-01-12T20:00:00-10:00'),
+      expected: '2018-01-13T06:00:00.000Z',
+      description: 'a datetime string with timezone information (- negative)',
+    },
+    {
+      datetime: new Date('2018-01-12T00:00:01'),
+      expected: '2018-01-11T23:00:01.000Z',
+      description: 'a datetime string in local time (mocked to `Europe/Paris`)',
+    },
+  ])(
+    'can use date object that is constructed with $description that will be converted to UTC to append datetime data',
+    async ({ datetime, expected }) => {
+      const serverSpy = jest.fn().mockReturnValue('{}');
+      mockPool
+        .intercept({
+          method: 'POST',
+          path: '/datasets/steps.by.day/data',
+        })
+        .reply(200, serverSpy);
+      const dataset = prepareDataset();
+
+      await dataset.append([
+        {
+          timestamp: datetime,
+          steps: 8190,
+        },
+      ]);
+      expect(serverSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          method: 'POST',
+          path: '/datasets/steps.by.day/data',
+          headers: expect.objectContaining({
+            authorization: `Basic ${btoa('API_KEY:')}`,
+            'user-agent': 'Geckoboard Node Client 2.0.0',
+            'content-type': 'application/json',
+          }),
+          body: JSON.stringify({
+            data: [
+              {
+                timestamp: expected,
+                steps: 8190,
+              },
+            ],
+          }),
         }),
-        body: JSON.stringify({
-          data: [
-            {
-              timestamp: '2018-01-01T12:00:00.000Z',
-              day: '2018-01-01',
-              steps: 819,
-            },
-            {
-              timestamp: '2018-01-02T12:00:00.000Z',
-              day: '2018-01-02',
-              steps: 409,
-            },
-            {
-              timestamp: '2018-01-03T12:00:00.000Z',
-              day: '2018-01-03',
-              steps: 164,
-            },
-          ],
+      );
+    },
+  );
+
+  it.each([
+    {
+      date: new Date('2018'),
+      expected: '2018-01-01',
+      description: 'just a year string',
+    },
+    {
+      date: new Date('2018-02'),
+      expected: '2018-02-01',
+      description: 'just a month string',
+    },
+    {
+      date: new Date('2018-03-02'),
+      expected: '2018-03-02',
+      description: 'just a date string',
+    },
+    {
+      date: new Date('2018-04-05T12:00:00.000Z'),
+      expected: '2018-04-05',
+      description: 'a UTC datetime string',
+    },
+    {
+      date: new Date('2013-09-15T05:53:00+08:00'),
+      expected: '2013-09-14',
+      description: 'a datetime string with timezone information (+ positve)',
+    },
+    {
+      date: new Date('2018-01-12T20:00:00-10:00'),
+      expected: '2018-01-13',
+      description: 'a datetime string with timezone information (- negative)',
+    },
+    {
+      date: new Date('2018-01-12T00:00:01'),
+      expected: '2018-01-11',
+      description: 'a datetime string in local time (mocked to `Europe/Paris`)',
+    },
+  ])(
+    'can use date object that is constructed with $description that will be converted to UTC to append date data',
+    async ({ date, expected }) => {
+      const serverSpy = jest.fn().mockReturnValue('{}');
+      mockPool
+        .intercept({
+          method: 'POST',
+          path: '/datasets/steps.by.day/data',
+        })
+        .reply(200, serverSpy);
+      const dataset = prepareDataset();
+
+      await dataset.append([
+        {
+          timestamp: new Date('2018-01-01T12:00:00Z'),
+          day: date,
+          steps: 819,
+        },
+      ]);
+      expect(serverSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          method: 'POST',
+          path: '/datasets/steps.by.day/data',
+          headers: expect.objectContaining({
+            authorization: `Basic ${btoa('API_KEY:')}`,
+            'user-agent': 'Geckoboard Node Client 2.0.0',
+            'content-type': 'application/json',
+          }),
+          body: JSON.stringify({
+            data: [
+              {
+                timestamp: '2018-01-01T12:00:00.000Z',
+                day: expected,
+                steps: 819,
+              },
+            ],
+          }),
         }),
-      }),
-    );
-  });
+      );
+    },
+  );
 
   it('will error if there is an issue appending to a dataset', async () => {
     mockPool
@@ -394,63 +490,159 @@ describe('Geckoboard', () => {
     );
   });
 
-  it('can replace data for a dataset using the Date object', async () => {
-    const serverSpy = jest.fn().mockReturnValue('{}');
-    mockPool
-      .intercept({
-        method: 'PUT',
-        path: '/datasets/steps.by.day/data',
-      })
-      .reply(200, serverSpy);
-    const dataset = prepareDataset();
-    await dataset.replace([
-      {
-        timestamp: new Date('2018-01-01T12:00:00Z'),
-        day: new Date('2018-01-01'),
-        steps: 819,
-      },
-      {
-        timestamp: new Date('2018-01-02T12:00:00Z'),
-        day: new Date('2018-01-02'),
-        steps: 409,
-      },
-      {
-        timestamp: new Date('2018-01-03T12:00:00Z'),
-        day: new Date('2018-01-03'),
-        steps: 164,
-      },
-    ]);
-    expect(serverSpy).toHaveBeenCalledWith(
-      expect.objectContaining({
-        method: 'PUT',
-        path: '/datasets/steps.by.day/data',
-        headers: expect.objectContaining({
-          authorization: `Basic ${btoa('API_KEY:')}`,
-          'user-agent': 'Geckoboard Node Client 2.0.0',
-          'content-type': 'application/json',
+  it.each([
+    {
+      datetime: new Date('2018'),
+      expected: '2018-01-01T00:00:00.000Z',
+      description: 'just a year string',
+    },
+    {
+      datetime: new Date('2018-02'),
+      expected: '2018-02-01T00:00:00.000Z',
+      description: 'just a month string',
+    },
+    {
+      datetime: new Date('2018-03-02'),
+      expected: '2018-03-02T00:00:00.000Z',
+      description: 'just a date string',
+    },
+    {
+      datetime: new Date('2018-04-05T12:00:00.000Z'),
+      expected: '2018-04-05T12:00:00.000Z',
+      description: 'a UTC datetime string',
+    },
+    {
+      datetime: new Date('2013-09-15T05:53:00+08:00'),
+      expected: '2013-09-14T21:53:00.000Z',
+      description: 'a datetime string with timezone information (+ positve)',
+    },
+    {
+      datetime: new Date('2018-01-12T20:00:00-10:00'),
+      expected: '2018-01-13T06:00:00.000Z',
+      description: 'a datetime string with timezone information (- negative)',
+    },
+    {
+      datetime: new Date('2018-01-12T00:00:01'),
+      expected: '2018-01-11T23:00:01.000Z',
+      description: 'a datetime string in local time (mocked to `Europe/Paris`)',
+    },
+  ])(
+    'can use date object that is constructed with $description that will be converted to UTC to replace datetime data',
+    async ({ datetime, expected }) => {
+      const serverSpy = jest.fn().mockReturnValue('{}');
+      mockPool
+        .intercept({
+          method: 'PUT',
+          path: '/datasets/steps.by.day/data',
+        })
+        .reply(200, serverSpy);
+      const dataset = prepareDataset();
+
+      await dataset.replace([
+        {
+          timestamp: datetime,
+          steps: 8190,
+        },
+      ]);
+      expect(serverSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          method: 'PUT',
+          path: '/datasets/steps.by.day/data',
+          headers: expect.objectContaining({
+            authorization: `Basic ${btoa('API_KEY:')}`,
+            'user-agent': 'Geckoboard Node Client 2.0.0',
+            'content-type': 'application/json',
+          }),
+          body: JSON.stringify({
+            data: [
+              {
+                timestamp: expected,
+                steps: 8190,
+              },
+            ],
+          }),
         }),
-        body: JSON.stringify({
-          data: [
-            {
-              timestamp: '2018-01-01T12:00:00.000Z',
-              day: '2018-01-01',
-              steps: 819,
-            },
-            {
-              timestamp: '2018-01-02T12:00:00.000Z',
-              day: '2018-01-02',
-              steps: 409,
-            },
-            {
-              timestamp: '2018-01-03T12:00:00.000Z',
-              day: '2018-01-03',
-              steps: 164,
-            },
-          ],
+      );
+    },
+  );
+
+  it.each([
+    {
+      date: new Date('2018'),
+      expected: '2018-01-01',
+      description: 'just a year string',
+    },
+    {
+      date: new Date('2018-02'),
+      expected: '2018-02-01',
+      description: 'just a month string',
+    },
+    {
+      date: new Date('2018-03-02'),
+      expected: '2018-03-02',
+      description: 'just a date string',
+    },
+    {
+      date: new Date('2018-04-05T12:00:00.000Z'),
+      expected: '2018-04-05',
+      description: 'a UTC datetime string',
+    },
+    {
+      date: new Date('2013-09-15T05:53:00+08:00'),
+      expected: '2013-09-14',
+      description: 'a datetime string with timezone information (+ positve)',
+    },
+    {
+      date: new Date('2018-01-12T20:00:00-10:00'),
+      expected: '2018-01-13',
+      description: 'a datetime string with timezone information (- negative)',
+    },
+    {
+      date: new Date('2018-01-12T00:00:01'),
+      expected: '2018-01-11',
+      description: 'a datetime string in local time (mocked to `Europe/Paris`)',
+    },
+  ])(
+    'can use date object that is constructed with $description that will be converted to UTC to replace date data',
+    async ({ date, expected }) => {
+      const serverSpy = jest.fn().mockReturnValue('{}');
+      mockPool
+        .intercept({
+          method: 'PUT',
+          path: '/datasets/steps.by.day/data',
+        })
+        .reply(200, serverSpy);
+      const dataset = prepareDataset();
+
+      await dataset.replace([
+        {
+          timestamp: new Date('2018-01-01T12:00:00Z'),
+          day: date,
+          steps: 819,
+        },
+      ]);
+      expect(serverSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          method: 'PUT',
+          path: '/datasets/steps.by.day/data',
+          headers: expect.objectContaining({
+            authorization: `Basic ${btoa('API_KEY:')}`,
+            'user-agent': 'Geckoboard Node Client 2.0.0',
+            'content-type': 'application/json',
+          }),
+          body: JSON.stringify({
+            data: [
+              {
+                timestamp: '2018-01-01T12:00:00.000Z',
+                day: expected,
+                steps: 819,
+              },
+            ],
+          }),
         }),
-      }),
-    );
-  });
+      );
+    },
+  );
 
   it('will error if there is an issue replacing a dataset', async () => {
     mockPool
